e42e7e363eecb758f902ccedad03bf90a4c0fa71
Christopher Webber <cwebber@getchef.com>
2014-05-02 21:55:51 -0700

    Initial commit of a generator thingy

---
+++ README.md
@@ -0,0 +1,5 @@
+# Supermarket Update Generator
+
+This generates the various lists that go in the weekly Supermarket Update.
+
+Mostly, it is just an easy way to grab all the GitHub PRs and Trello Cards.
---
+++ generate.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'octokit'
+require 'date'
+require 'trello'
+
+github = Octokit::Client.new(:access_token => ENV['GITHUB_API_KEY'])
+week_ago = Date.today - 7
+
+puts "Tasks Completed This Week"
+github.pull_requests('opscode/supermarket', :state => 'closed').each do |pr|
+
+  if pr[:merged_at].to_date > week_ago
+    puts "* #{pr[:title]} - (#{pr[:html_url]})"
+  end
+end
+
+puts "You can view our complete list of Pull Requests here: https://github.com/opscode/supermarket/pulls"
+puts ""
+
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_APP_KEY']
+  config.member_token = ENV['TRELLO_USER_TOKEN']
+end
+
+doing = Trello::List.find('532b17091b4da9e958dad21c').cards
+todo = Trello::List.find('532b17091b4da9e958dad21b').cards
+
+puts "Tasks Currently Being Worked On"
+doing.each do |card|
+  puts "* #{card.name} - (#{card.url})"
+end
+
+puts ""
+
+puts "Tasks That Are Up Next"
+todo.each do |card|
+  puts "* #{card.name} - (#{card.url})"
+end
+
+puts "The full Trello board can be viewed here:  https://trello.com/b/IGLbkBWL/supermarket"
